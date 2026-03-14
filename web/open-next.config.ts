966d3f412d760ca5fc27e7d5a6ae70df9f95881a
uncle-brother-son <wayne@unclebrotherson.com>
2026-03-14 10:36:17 +0000

    Add D1 tag cache for revalidatePath support

--- web/open-next.config.ts
+++ web/open-next.config.ts
@@ -1,8 +1,11 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 
 export default defineCloudflareConfig({
 	// Enable KV caching for ISR
-	incrementalCache: kvIncrementalCache
+	incrementalCache: kvIncrementalCache,
+	// Enable D1 tag cache for on-demand revalidation (revalidatePath/revalidateTag)
+	tagCache: d1NextTagCache
 });
