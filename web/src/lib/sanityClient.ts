2f94356e66b3ef0d7fcfc618ae2e2b0c41eec656
uncle-brother-son <wayne@unclebrotherson.com>
2026-03-21 13:44:09 +0000

    Cache test

--- web/src/lib/sanityClient.ts
+++ web/src/lib/sanityClient.ts
@@ -4,6 +4,6 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '23z8qxu4',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01',
-  useCdn: false, // Disable CDN for instant updates (ISR handles caching)
+  useCdn: false, // Disable CDN for instant updates (ISR handles caching) change for push
   perspective: 'published',
 })
